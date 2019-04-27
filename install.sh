61797d511cb7dd8028f74faf33e000dde7f482f8
Paul <517935q@gmail.com>
2019-04-27 23:42:16 +0300

    Added chmod and chown to install script

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+sudo chmod +x init-git-local.sh
 
 sudo ln -s $pwd/init-git-local.sh /usr/bin/ginlo
+
+sudo chown $(whoami):$(whoami) /usr/bin/ginlo
 
 echo "Successfully installed. Try to run 'ginlo'"
