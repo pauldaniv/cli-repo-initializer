967a033efc66955f640f8ee87623cffa48759219
Paul <paulsegfault@gmail.com>
2020-02-03 00:40:37 +0200

    Fixed origin and all remotes setup

--- init-git-local.sh
+++ init-git-local.sh
@@ -19,26 +19,29 @@ bitbucket_repo_url="git@bitbucket.org:miamibeach87/$current_folder_name.git"
 github_repo_url="git@github.com:pauldaniv/$current_folder_name.git"
 gitlab_repo_url="git@gitlab.com:miamibeach87/$current_folder_name.git"
 
-git remote add origin $bitbucket_repo_url
-git remote add all $bitbucket_repo_url
+if [[ $(check_remote_exists origin "$bitbucket_repo_url") ]]; then
+  git remote add origin $bitbucket_repo_url
+fi
+if [[ $(check_remote_exists all "$bitbucket_repo_url") ]]; then
+  git remote add all $bitbucket_repo_url
+  check_added_successfully "Bitbucket"
+fi
 
-check_added_successfully "Bitbucket"
-
-if [[ $(check_remote_exists github "$gitlab_repo_url") ]]; then
+if [[ $(check_remote_exists github "$github_repo_url") ]]; then
   git remote add github $github_repo_url
 fi
 if [[ $(check_remote_exists all "$github_repo_url") ]]; then
   git remote set-url --add --push all $github_repo_url
+  check_added_successfully "Github"
 fi
-check_added_successfully "Github"
 
 if [[ $(check_remote_exists gitlab "$gitlab_repo_url") ]]; then
   git remote add gitlab $gitlab_repo_url
 fi
 if [[ $(check_remote_exists all "$gitlab_repo_url") ]]; then
   git remote set-url --add --push all $gitlab_repo_url
+  check_added_successfully "Gitlab"
 fi
-check_added_successfully "Gitlab"
 
 # need to make this last (or at least not first after adding 'all' remote) becouse first push url setting will override
 # automatically created push url
