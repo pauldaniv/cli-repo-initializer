3443b019cddd7c0e87ea9e4f5d5b6dc61ea87040
Paul <paulsegfault@gmail.com>
2020-08-14 03:24:14 +0300

    Added env variable use instead of manual input

--- tools/gitinit.sh
+++ tools/gitinit.sh
@@ -15,6 +15,8 @@ check_remote_exists() {
 readValue() {
   READ_VALUE_RESULT=
   local value
+
+
   echo "$1: " | tr -d '\n'
 
   local CHECK_MARK="\033[0;32m\xE2\x9C\x94\033[0m"
@@ -35,52 +37,68 @@ git init
 git config --replace-all user.ssh.private.key.name ~/.ssh/id_rsa
 
 readValue "Update user info?"
-if [[ "$READ_VALUE_RESULT" ]]; then
-  readValue "Enter ssh key file path"
+if [[ "$READ_VALUE_RESULT" = "y" ]]; then
+  if [[ "$GIT_SSH_KEY_PATH" ]]; then
+    READ_VALUE_RESULT=$GIT_SSH_KEY_PATH
+  else
+    readValue "Enter ssh key file path"
+  fi
   [[ "$READ_VALUE_RESULT" ]] && git config --replace-all user.ssh.private.key.name "$READ_VALUE_RESULT"
-  readValue "Enter user.email"
+  if [[ "$GIT_DEFAULT_USER_EMAIL" ]]; then
+    READ_VALUE_RESULT=$GIT_DEFAULT_USER_EMAIL
+  else
+    readValue "Enter user.email"
+  fi
   [[ "$READ_VALUE_RESULT" ]] && git config user.email "$READ_VALUE_RESULT"
-  readValue "Enter user.name"
+  if [[ "$GIT_DEFAULT_USER_NAME" ]]; then
+    READ_VALUE_RESULT=$GIT_DEFAULT_USER_NAME
+  else
+    readValue "Enter user.name"
+  fi
   [[ "$READ_VALUE_RESULT" ]] && git config user.name "$READ_VALUE_RESULT"
 fi
 
 git config user.signingkey ''
 git config commit.gpgsign ''
 
-bitbucket_repo_url="git@bitbucket.org:miamibeach87/$current_folder_name.git"
-github_repo_url="git@github.com:pauldaniv/$current_folder_name.git"
-gitlab_repo_url="git@gitlab.com:miamibeach87/$current_folder_name.git"
+github_repo_url="git@github.com:$GITHUB_USER_NAME/$current_folder_name.git"
 
 git remote -v | awk '{print $1}' | uniq | xargs -n1 git remote remove
 
-if [[ $(check_remote_exists origin "$bitbucket_repo_url") ]]; then
-  git remote add origin $bitbucket_repo_url
-fi
-if [[ $(check_remote_exists all "$bitbucket_repo_url") ]]; then
-  git remote add all $bitbucket_repo_url
-  check_added_successfully "Bitbucket"
-fi
-
-if [[ $(check_remote_exists github "$github_repo_url") ]]; then
-  git remote add github $github_repo_url
+if [[ $(check_remote_exists origin "$github_repo_url") ]]; then
+  git remote add origin $github_repo_url
 fi
 if [[ $(check_remote_exists all "$github_repo_url") ]]; then
-  git remote set-url --add --push all $github_repo_url
-  check_added_successfully "Github"
+  git remote add all $github_repo_url
+  check_added_successfully "GitHub"
 fi
 
-if [[ $(check_remote_exists gitlab "$gitlab_repo_url") ]]; then
-  git remote add gitlab $gitlab_repo_url
-fi
-if [[ $(check_remote_exists all "$gitlab_repo_url") ]]; then
-  git remote set-url --add --push all $gitlab_repo_url
-  check_added_successfully "Gitlab"
+if [[ "$BITBUCKET_USER_NAME" ]]; then
+  bitbucket_repo_url="git@bitbucket.org:$BITBUCKET_USER_NAME/$current_folder_name.git"
+  if [[ $(check_remote_exists bucket "$bitbucket_repo_url") ]]; then
+    git remote add bucket $bitbucket_repo_url
+  fi
+  if [[ $(check_remote_exists all "$bitbucket_repo_url") ]]; then
+    git remote set-url --add --push all $bitbucket_repo_url
+    check_added_successfully "Bitbucket"
+  fi
 fi
 
-# need to make this last (or at least not first after adding 'all' remote) becouse first push url setting will override
+if [[ "$GITLAB_USER_NAME" ]]; then
+  gitlab_repo_url="git@gitlab.com:$GITLAB_USER_NAME/$current_folder_name.git"
+  if [[ $(check_remote_exists gitlab "$gitlab_repo_url") ]]; then
+    git remote add gitlab $gitlab_repo_url
+  fi
+  if [[ $(check_remote_exists all "$gitlab_repo_url") ]]; then
+    git remote set-url --add --push all $gitlab_repo_url
+    check_added_successfully "Gitlab"
+  fi
+fi
+
+# need to make this last (or at least not first after adding 'all' remote) because first push url setting will override
 # automatically created push url
-if [[ $(check_remote_exists all "$bitbucket_repo_url") ]]; then
-  git remote set-url --add --push all $bitbucket_repo_url
+if [[ $(check_remote_exists all "$github_repo_url") ]]; then
+  git remote set-url --add --push all "$github_repo_url"
 fi
 
 echo
