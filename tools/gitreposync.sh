39e59ace7f58c92d8f21337eb845efe17ea45b86
Paul <paulsegfault@gmail.com>
2020-08-25 00:36:11 +0300

    Added commented lines handling

--- tools/gitreposync.sh
+++ tools/gitreposync.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-
+IFS=$(echo -en "\n\b")
 for i in $(cat $REPOS_LIST_FILE); do
-  cd "$REPOS_BASE_PATH/$i"
-  gitcom pull all develop
+  if [[ $i && ! $i =~ ^[[:space:]]*\#.*$ ]]; then
+    cd "$REPOS_BASE_PATH/$i"
+    echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+    echo "Synching `pwd`"
+    gitcom pull all develop
+  fi
 done
+IFS=$SAVEIFS
